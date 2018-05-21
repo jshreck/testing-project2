f0e124145f17787f5391d83a0935e25ff2f3a116
Julia Shreckhise <julia.shreckhise@gmail.com>
2018-05-21 19:51:26 -0400

    added too newPost

--- public/js/newPost.js
+++ public/js/newPost.js
@@ -3,9 +3,11 @@ $(document).ready(() => {
 
 $(document).on("click", "#createPostSubmit", (clicked) => {
 
+
     event.preventDefault();
 
     var userID = $(clicked.currentTarget).data("userid");
+    var username = $("#username").data("username");
 
     console.log("userID " + userID);
 
@@ -14,8 +16,10 @@ $(document).on("click", "#createPostSubmit", (clicked) => {
     
 
      //sends form data
-     $.post(`/api/newPost/${userID}`, data).then((response) => {
+     $.post(`/api/newPost/${userID}`, data)
+     .then((response) => {
          console.log(response);
+         window.location.replace("/blogspace/" + username);
      })
          .catch((err) => {
              alert(err.responseText);
