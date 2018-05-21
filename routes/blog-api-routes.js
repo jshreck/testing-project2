f074993676d39f30af87403b6f0343800d34957f
Julia Shreckhise <julia.shreckhise@gmail.com>
2018-05-20 22:31:10 -0400

    laying out route logic in blog-api

--- routes/blog-api-routes.js
+++ routes/blog-api-routes.js
@@ -1,24 +1,33 @@
 var db = require("../models");
 
 module.exports = function (app) {
-    //creating blog post
+
+    //creating blog post =============================NOT YET TESTED
     app.post("api/newPost", (req, res) => {
-        db.BlogPost.create(req.body).then((newPost) => {
+        //NEED TO GET TITLE, BODY, USERID IN REQ
+        db.BlogPost.create(
+            {
+                title: req.body.title,
+                body: req.body.body,
+                UserId: req.body.userID
+            }
+        ).then((newPost) => {
             res.json(newPost);
         });
     });
 
-    //updating blog post
+    //updating blog post ===========================NOT YET TESTED
     app.put("api/update/:postID", (req, res) => {
-        db.BlogPost.update(
-            req.body,
-            {
-                where: {
-                    id: req.body.id
-                }
-            }).then((updatedPost) => {
-                res.json(updatedPost);
-            });
+        // NEED TITLE AND BODY
+        db.BlogPost.update({
+            title: title,
+            body: body,
+            where: {
+                id: req.params.id,
+            }
+        }).then((updatedPost) => {
+            res.json(updatedPost);
+        });
     });
 
     //deleting blog post
@@ -30,6 +39,19 @@ module.exports = function (app) {
             }
         }).then((numDeleted) => {
             res.status(200).send("success");
+        });
+    });
+
+    //choosing template ===================================NOT YET TESTED
+    app.put("api/template/:templateID", (req, res) => {
+        // NEED USER ID (id)
+        db.User.update({
+            template: req.params.templateID,
+            where: {
+                id: id,
+            }
+        }).then((user) => {
+            res.json(user);
         });
     });
 
